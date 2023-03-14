4f964dc43ef3d1b562dd250feef296ae8c6e30e9
Kawshal29 <93502457+Kawshal29@users.noreply.github.com>
2023-03-14 15:42:18 +0530

    Add files via upload

---
+++ 02_1_variabletype.py
@@ -0,0 +1,19 @@
+a = 20
+b = 30.56
+c = "Kawshal"
+d = None
+e = '''Shirdhankar'''
+
+# Printing variables values
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# printing type of variable
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
